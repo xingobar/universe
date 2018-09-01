387d48a5445bc7d2f7080711a625e1f389f1b40e
xingobar <gary840227@gmail.com>
2018-09-01 20:23:08 +0700

    1.增加太陽＆星空調整

--- src/index.js
+++ src/index.js
@@ -4,14 +4,24 @@ var renderer; // 渲染
 var geometry;
 var material; // 材質
 var earth_mesh; // 地球
+var controls; // 控制元件
+var light; // 燈光
+var sun_mesh; // 太陽
 
 function init() {
 	scene = new THREE.Scene(); //建立場景
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); // fov , aspect, near ,far
+	camera = new THREE.PerspectiveCamera(140, window.innerWidth / window.innerHeight, 0.1, 1000); // fov , aspect, near ,far
 	renderer = new THREE.WebGLRenderer({ alpha: true });
 	renderer.setSize(window.innerWidth, window.innerHeight); // 設定場景大小
 	scene.background = new THREE.Color(0xffffff); // 設定場景背景色
 	document.body.appendChild(renderer.domElement);
+
+	camera.position.set(0, 3, 0); // 設定相機位置
+
+	// 設定軌道運行控制
+	controls = new THREE.OrbitControls(camera);
+	controls.autoRotate = true;
+	controls.update();
 
 	var loader = new THREE.TextureLoader();
 	loader.load(
@@ -51,27 +61,28 @@ function init() {
 			 */
 			earth_geometry = new THREE.SphereGeometry(2, 64, 64); // radius, widthFragment, heightFragment
 			earth_mesh = new THREE.Mesh(earth_geometry, earth_material); // 建立物件
-			earth_mesh.position.set(0, 0, 0); // x,y,z
+			earth_mesh.position.set(5, 0, 0); // x,y,z
 
 			earth_mesh.add(cloud_mesh);
 
-			scene.add(earth_mesh); // 將地球加入場景
+			scene.add(earth_mesh); //將地球加入場景;
 
 			/**
 			 * 加入星空
 			 */
-			var star_geometry = new THREE.SphereGeometry(200, 64, 64);
+			var star_geometry = new THREE.SphereGeometry(10, 64, 64);
 			var star_material = new THREE.MeshBasicMaterial({
-				map: new THREE.TextureLoader().load('/universe/static/img/galaxy_starfield.png'),
+				map: new THREE.TextureLoader().load('/universe/static/img/StarsMap.jpg'),
 				side: THREE.BackSide
 			});
 			var star_mesh = new THREE.Mesh(star_geometry, star_material);
 			scene.add(star_mesh); // 將星空加入場景
+
 			/**
 			 * meshPhongMaterial 要加入燈光，否則會顯示不出來
 			 */
-			var light = new THREE.DirectionalLight(0xffffff);
-			light.position.set(0, 1, 1).normalize();
+			light = new THREE.DirectionalLight(0xffffff);
+			light.position.set(1, 1, 1).normalize();
 			scene.add(light); // 加入燈光
 		},
 		undefined, // onPress callback
@@ -80,6 +91,18 @@ function init() {
 			console.log(err);
 		}
 	);
+
+	// 增加太陽
+	loader.load('/universe/static/img/sun.jpg', function(texture) {
+		var sun_material = new THREE.MeshPhongMaterial({
+			map: texture
+		});
+
+		var sun_geometry = new THREE.SphereGeometry(2, 64, 64);
+		sun_mesh = new THREE.Mesh(sun_geometry, sun_material);
+		sun_mesh.position.set(0, 0, 0);
+		scene.add(sun_mesh);
+	});
 
 	camera.position.z = 5;
 }
@@ -91,6 +114,12 @@ var animate = function() {
 		earth_mesh.rotation.y += 0.01;
 	}
 
+	if (sun_mesh) {
+		sun_mesh.rotation.y += 0.01;
+		light.position.copy(camera.getWorldPosition());
+	}
+
+	controls.update();
 	renderer.render(scene, camera);
 };
 init();
